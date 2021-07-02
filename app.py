f4bf1cf7760a668a028366a6239956316f74c8e3
dkubar <darina.kubar@gmail.com>
2021-07-02 10:27:09 +0200

    add you-might-also-like function

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from geopy.distance import geodesic
 import pandas as pd
 from flask import Flask, jsonify, request
+import json
 
+# Function to get street art close to a set of coordinates
 def get_close_objects(lat, lng):
     input_coord = (lat, lng)
     df_final = pd.read_csv('data_final.csv')
@@ -13,8 +15,20 @@ def get_close_objects(lat, lng):
             result.append({"id": index, "distance": distance})
     return result
 
+# Function to load list of dictionaries of artworks ranked by similarity
+def load_rankings():
+    with open('liked_rankings.json') as rankings_file:
+        similarity_dict = json.load(rankings_file)
+        return similarity_dict
+
 app = Flask(__name__)
 
+# Route homepage
+@app.route('/', methods=['GET'])
+def home():
+    return '''<h1>A prototype API for WSA 'You Might Also Like' and 'Near You' features.</h1><p></p>'''
+
+# Route "near-you" function
 @app.route('/near-you', methods = ['GET'])
 def near_you():
     latitude = request.args['lat']
@@ -23,19 +37,28 @@ def near_you():
     response.headers.add("Access-Control-Allow-Origin", "*")
     return response
 
+# Route "you may also like" function
+@app.route('/ymal', methods=['GET'])
+def api_id():
+    # Check if an ID was provided as part of the URL.
+    # If ID is provided, assign it to a variable.
+    # If no ID is provided, display an error in the browser.
+    if 'id' in request.args:
+        id = request.args['id']
+    else:
+        return "Error: No id field provided. Please specify an id."
+
+    # Load list of dictionaries of artworks ranked by similarity.
+    rankings = load_rankings()
+    # Create an empty list for results
+    results = []
+    # Loop through the data and match 5 closest results that fit the requested ID.
+    for i in range(5):
+                similar_artwork = rankings[id][i]
+                results.append(similar_artwork)
+    response = jsonify(results)
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
+
 if __name__ == '__main__':
     app.run(debug=True)
-
-# @app.route('/near-you', methods = ['GET'])
-# def near_you():
-#     latitude = request.args['lat']
-#     longitude = request.args["lng"]
-#     result = get_close_objects(latitude, longitude)
-#     return jsonify(result)
-#
-#     # Enable Access-Control-Allow-Origin
-#     result.headers.add("Access-Control-Allow-Origin", "*")
-#     return result
-#
-# if __name__ == '__main__':
-#     app.run(debug=True)
